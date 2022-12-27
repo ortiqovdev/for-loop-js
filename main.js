678451c4122cd62a9256436d8852bdf93f3a9d45
ortiqovdev <akmalbekortiqov7976@gmail.com>
2022-12-27 13:04:55 +0500

    for loop 1-17

---
+++ main.js
@@ -0,0 +1,160 @@
+// * For sikl operatoriga oid masalalar
+// * date: 27.12.2022  ,  10:40
+
+// for1
+// let k = 10
+// let n = 8
+// for (let i = 0; i  < n; i++){
+//     console.log(k);
+// }
+
+// for2
+// let a=10
+// let b=15
+// for (let a=10 ; a<=b ; a++){
+//     console.log(a);
+// }
+
+// for3
+// let a = 10;
+// let b = 20;
+// for (let i = b; i >= a; i--) {
+//     console.log(i);
+// }
+
+// for4
+// let a=5_000
+// for(let i=1 ; i<10 ; i++){
+//     console.log(i*a);
+// }
+
+// for5
+// let a=50_000
+// for (let i=0.1; i<1 ; i=i+0.1){
+//     console.log(a*i.toFixed(1));
+// }
+
+// for6
+// let a=5_000
+// for(let i=1; i <2 ; i=i+0.2){
+//     console.log(a*i.toFixed(1));
+// }
+
+// for7
+// let b = 20
+// let x = 0
+// for (let a = 10; a <= b; a++) {
+//     console.log(a);
+//     x = x + a
+//     console.log(x);
+// }
+// console.log(x);
+
+// for8
+// let b = 20
+// let x = 1
+// for (let a = 10; a <= b; a++) {
+//     console.log(a);
+//     x = x * a
+//     console.log(x);
+// }
+// console.log(x);
+
+// for9
+// let b = 15
+// let x = 1
+// for (let a = 10; a <= b; a++) {
+//     console.log(a);
+//     x = a * a
+//     console.log(x);
+// }
+// console.log(x);
+
+// for10
+// let s=0
+// let n=5
+// for (let i=1; i<=n; i++){
+//     console.log(i);
+//     console.log(1/i);
+//     s += 1 / i
+// }
+// console.log(s);
+
+// for11
+// let s = 0
+// let n = 5
+// for (let i = 1; i <= n; i++) {
+//     s += (n+i)**2
+//     console.log(s);
+// }
+// console.log(s);
+
+// for12
+// let k = 1
+// let n = 2
+// for (let i = 1; i <= n; i = i + 0.1) {
+//     k *= i * n
+//     console.log(k.toFixed(1));
+// }
+// console.log(k.toFixed(1));
+
+// for13
+// let k = 0
+// let n = 2
+// let a=1
+// for (let i = 1; i <= n; i = i + 0.1) {
+//     k += i * a
+//     console.log(k.toFixed(1));
+// a*= -1
+// }
+// console.log(k.toFixed(a));
+
+// for14
+// let n=5 
+// let x=0
+// let s=1
+// for(let i=1 ; i<=n ; i=i+2){
+// s+=(2*i-1)
+// console.log(s);
+// } 
+// console.log(s);
+
+// for15
+// let a=10
+// let n=3
+// let s=1
+// for(let i=1; i<=n; i++){
+//     console.log(a**i);
+// }
+
+// for16
+// let n=5
+// let a=3
+// let s=1
+// for(let i=1;  i<=n; i++){
+// s*=a
+// console.log(s);
+// }
+// console.log(s);
+
+// for17
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export{}
